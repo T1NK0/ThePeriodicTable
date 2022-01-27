a73438aae0adbc954c7e1d054983361dd5135353
Mads Tinko Madsen <mtm@itklyngen.dk>
2022-01-27 12:37:10 +0100

    setting up periodic table

---
+++ src/app/interfaces/cell.ts
@@ -0,0 +1,22 @@
+export interface Cell {
+    atomicMass: '';
+    atomicNumber: '';
+    atomicRadius: '';
+    boilingPoint: '';
+    bondingType: '';
+    cpkHexColor: '';
+    density: '';
+    electronAffinity: '';
+    electronegativity: '';
+    electronicConfiguration: '';
+    groupBlock: '';
+    ionRadius: '';
+    ionizationEnergy: '';
+    meltingPoint: '';
+    name: '';
+    oxidationStates: '';
+    standardState: '';
+    symbol: '';
+    vanDelWaalsRadius: '';
+    yearDiscovered: '';
+}
