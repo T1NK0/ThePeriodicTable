7e8b19059e3500901c782a2bfbf2d41a8b3e84a0
Mads Tinko Madsen <lyderslev1@gmail.com>
2022-01-27 19:39:11 +0100

    setup basics for periodic table

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Atom } from '../Atom';
+import { AtomsService } from '../services/atoms.service';
 
 @Component({
   selector: 'app-periodic-table',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeriodicTableComponent implements OnInit {
 
-  constructor() { }
+  atoms: Atom[] = [];
 
+  //Calls our AtomsService, we need to get the atoms from the URL.
+  //Constructor gets run multiple times, where as ngOnInit get runned once.
+  constructor(private AtomsService: AtomsService) { }
+
+  //get's runned once.
   ngOnInit(): void {
-  }
+    //Run our getAtoms service.
+    this.AtomsService.getAtoms().subscribe(periodicTableData => {
+      //sets our api data as "periodicTabelData", which we can make a for loop on, to look at each record in the json file.
+        for(let dataAtom of periodicTableData) {
+          //Create a new atom from our atom class
+          let atom = new Atom ();
+            atom.atomicMass = dataAtom.atomicMass;
+            atom.atomicNumber = dataAtom.atomicNumber;
+            atom.name = dataAtom.name;
+            atom.symbol = dataAtom.symbol;
+            this.atoms.push(atom);
+          }
+        }
+      )
+    }
 
-}
+  }
